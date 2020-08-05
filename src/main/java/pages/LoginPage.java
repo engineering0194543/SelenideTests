2488fe9c91a229f0e7909e23c1632f90a56c8695
username <username@domaim.com>
2020-08-05 12:21:42 +0300

    edited LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Condition;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
-//import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
 
